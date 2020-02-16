6a101aba18f40b1897dd956c5b57ef699405a568
sy0837 <50517233+sy0837@users.noreply.github.com>
2020-02-16 21:17:41 +0530

    update

--- js/index.js
+++ js/index.js
@@ -5,12 +5,20 @@
 $("#health").on({
     'click': function(){
         $('#content-img').attr('src','images/about_1.jpg');
-        $('.content-text').html("Yolo")
+        $('.content-text').html("Yolo");
+        $('#health').addClass("clicked");
+        setTimeout(function(){
+            $('#health').removeClass("clicked"); 
+        },100)
     }
 })
 $("#blood").on({
     'click': function(){
         $('#content-img').attr('src','images/bg-101.png');
-        $('.content-text').html("Hello World")
+        $('.content-text').html("Hello World");
+        $('#blood').addClass("clicked");
+        setTimeout(function(){
+            $('#blood').removeClass("clicked"); 
+        },100)
     }
 })
